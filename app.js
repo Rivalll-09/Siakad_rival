17b70cf133c12041b8f8919ea49e297b8227c56b
Rivalll-09 <valv8840@gmail.com>
2024-01-25 22:10:06 +0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const helmet = require("helmet");
+const cors = require("cors");
+const isAuth = require("./auth");
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth');
+
 
 const mahasiswaRoutes = require('./routes/mahasiswa');
 const jurusanRoutes = require('./routes/jurusan');
@@ -9,28 +14,33 @@ const dosenRoutes = require('./routes/dosen');
 const jadwalRoutes = require('./routes/jadwal');
 const matkulRoutes = require('./routes/matkul');
 
-app.use(morgan ('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
 
-app.use('/mahasiswa', mahasiswaRoutes);
-app.use('/jurusan', jurusanRoutes);
-app.use('/dosen', dosenRoutes);
-app.use('/jadwal', jadwalRoutes);
-app.use('/matkul', matkulRoutes);
+app.use('/mahasiswa', isAuth, mahasiswaRoutes);
+app.use('/jurusan', isAuth, jurusanRoutes);
+app.use('/dosen', isAuth, dosenRoutes);
+app.use('/jadwal', isAuth, jadwalRoutes);
+app.use('/matkul', isAuth, matkulRoutes);
 
-app.use((req, res, next) =>{
-    const error = new Error('Tidak ada');
-    error.status = 404;
-    next(error);
-})
+app.use("/request", (_, res) => {
+    const response = {
+        apiKey: process.env.API_KEY,
+    };
+    res.status(201).json(response);
+});
 
-app.use((error, req, res, next) =>{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            messsage: error.message
-        }
+app.use("/", (_, res) =>
+    res.status(200).json({
+        status: 200,
+        name: "SIAKAD-API",
+        version: "1.0.0",
+        is_open: true,
     })
-})
+);
+
+
 module.exports = app;
